8c992f882a39a9d495871503507ebb31a6628dab
Admin <Admin@DESKTOP-TC9JO05>
2023-05-04 20:23:46 +0700

    Add project files.

---
+++ T2203E_Demo/Entities/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace T2203E_Demo.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options)
+        :base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ T2203E_Demo/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace T2203E_Demo.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required] 
+        public double Price { get; set; }
+
+        [StringLength(255)]
+        public string Thumbnail { get; set; }
+
+
+    }
+}
